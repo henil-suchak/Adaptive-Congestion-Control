f142804b8717fa7e7619270a248421e90a95f58d
Archii1201 <1201archii@gmail.com>
2026-01-13 22:23:20 +0530

    Implementation changes of experience controller

--- src/main/java/com/HAJ/congestion/controller/ExperimentController.java
+++ src/main/java/com/HAJ/congestion/controller/ExperimentController.java
@@ -1,42 +1,42 @@
 package com.HAJ.congestion.controller;
 
 import com.HAJ.congestion.entity.Experiment;
-import com.HAJ.congestion.entity.Flow;
 import com.HAJ.congestion.service.ExperimentService;
-import com.HAJ.congestion.service.FlowService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ExperimentController {
+
     private final ExperimentService experimentService;
 
     public ExperimentController(ExperimentService experimentService) {
         this.experimentService = experimentService;
     }
-    @PostMapping("/experiments/")
-    public Experiment createExperiment(@RequestBody  Experiment experiment) {
 
-        return experimentService.createExperiment(experiment.getName(),
+    @PostMapping("/experiments")
+    public Experiment createExperiment(@RequestBody Experiment experiment) {
+        return experimentService.createExperiment(
+                experiment.getName(),
                 experiment.getTopology(),
                 experiment.getBottleneckBandwidthMbps(),
                 experiment.getBaseDelayMs(),
-                experiment.getQueueType());
-    }
-    @PostMapping("/experiments/{experimentId}/start")
-    public void startExperiment(@PathVariable Long experimentId)
-    {
-         experimentService.startExperiment(experimentId);
-    }
-    @PostMapping("/experiments/{experimentId}/end")
-    public void endExperiment(@PathVariable Long experimentId)
-    {
-        experimentService.endExperiment(experimentId);
-    }
-    @GetMapping("/experiments/{experimentId}")
-    public Experiment getExperimentById(@PathVariable Long experimentId)
-    {
-        return experimentService.getExperimentById(experimentId);
+                experiment.getQueueType()
+        );
     }
 
+    @PostMapping("/experiments/{experimentId}/start")
+    public void startExperiment(@PathVariable Long experimentId) {
+        experimentService.startExperiment(experimentId);
+    }
+
+    @PostMapping("/experiments/{experimentId}/end")
+    public void endExperiment(@PathVariable Long experimentId) {
+        experimentService.endExperiment(experimentId);
+    }
+
+    @GetMapping("/experiments/{experimentId}")
+    public Experiment getExperimentById(@PathVariable Long experimentId) {
+        return experimentService.getExperimentById(experimentId);
+    }
 }
